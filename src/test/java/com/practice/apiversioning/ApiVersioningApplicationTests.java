a7c22d8e42c5f720991da169fbd2a22cdc029689
Laurel Lao Ang <laurellaoang@gmail.com>
2022-06-30 12:41:34 +0800

    Initial commit

---
+++ src/test/java/com/practice/apiversioning/ApiVersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.apiversioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVersioningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
